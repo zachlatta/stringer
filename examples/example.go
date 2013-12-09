214578afa15ca058ec40636901879db214e7d757
Zach Latta <zach@zachlatta.com>
2013-12-08 20:39:45 -0800

    Add usage example.

---
+++ examples/example.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+  "fmt"
+
+  "github.com/zachlatta/stringer/stringer"
+)
+
+func main() {
+  editDistance := stringer.LevenshteinDistance("foo", "bar")
+  fmt.Println(editDistance)
+}
