b86c1592a100420b94be72804ac71648f22496a6
Zach Latta <zach@zachlatta.com>
2013-12-08 20:08:16 -0800

    Implement recursive Levenshtein distance strategy.

---
+++ stringer/stringer.go
@@ -0,0 +1,36 @@
+package stringer
+
+func LevenshteinDistance(s string, t string) int {
+  return levenshteinDistance(s, len(s), t, len(t))
+}
+
+func levenshteinDistance(s string, lenS int, t string, lenT int) int {
+  if lenS == 0 {
+    return lenT
+  }
+  if lenT == 0 {
+    return lenS
+  }
+
+  var cost int
+
+  if s[lenS-1] == t[lenT-1] {
+    cost = 0
+  } else {
+    cost = 1
+  }
+
+  return min(levenshteinDistance(s, lenS - 1, t, lenT) + 1,
+    levenshteinDistance(s, lenS, t, lenT - 1) + 1,
+    levenshteinDistance(s, lenS - 1, t, lenT - 1) + cost)
+}
+
+func min(args ...int) int {
+  min := args[0]
+  for _, v := range args {
+    if min > v {
+      min = v
+    }
+  }
+  return min
+}
