7639e6d45e270df035a6e2845d7508b892986485
Zach Latta <zach@zachlatta.com>
2013-12-09 00:44:35 -0800

    Use iterative with two matrix rows stategy instead of naive recursive strategy for the Levenshtein distance implementation.

--- stringer/levenshteinDistance.go
+++ stringer/levenshteinDistance.go
@@ -1,26 +1,51 @@
 package stringer
 
 func LevenshteinDistance(s string, t string) int {
-  return levenshteinDistance(s, len(s), t, len(t))
-}
-
-func levenshteinDistance(s string, lenS int, t string, lenT int) int {
-  if lenS == 0 {
-    return lenT
+  // Degenerate cases
+  if s == t {
+    return 0
   }
-  if lenT == 0 {
-    return lenS
+  if len(s) == 0 {
+    return len(t)
   }
-
-  var cost int
-
-  if s[lenS-1] == t[lenT-1] {
-    cost = 0
-  } else {
-    cost = 1
+  if len(t) == 0 {
+    return len(s)
   }
 
-  return min(levenshteinDistance(s, lenS - 1, t, lenT) + 1,
-    levenshteinDistance(s, lenS, t, lenT - 1) + 1,
-    levenshteinDistance(s, lenS - 1, t, lenT - 1) + cost)
+  // Create two work vectors of integer distances
+  v0 := make([]int, len(t) + 1)
+  v1 := make([]int, len(t) + 1)
+
+  // Initialize v0 (previous row of distances)
+  // this row is A[0][i] - edit distance 
+  // the distance is just the number of characters to delete from t
+  for i := range v0 {
+    v0[i] = i;
+  }
+
+  // Calculate v1 (current row distances) from the previous row v0
+  for i := range s {
+    // First element of v1 is A[i+1][0]
+    // Edit distance is delete (i+1) chars from s to match empty t
+    v1[0] = i + 1
+
+    // Use formula to fill in the rest of the row
+    for j := range t {
+      var cost int
+      if s[i] == t[j] {
+        cost = 0
+      } else {
+        cost = 1
+      }
+
+      v1[j + 1] = min(v1[j] + 1, v0[j + 1] + 1, v0[j] + cost)
+    }
+
+    // Copy v1 (current row) to v0 (previous row) for next iteration
+    for j := range v0 {
+      v0[j] = v1[j]
+    }
+  }
+
+  return v1[len(t)]
 }
